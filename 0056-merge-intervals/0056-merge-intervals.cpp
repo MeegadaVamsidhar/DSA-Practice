800bb2751068489cfc366f2716acd0ecb7116922
Meegada Vamsidhar Reddy <meegadavamsi76@gmail.com>
2026-07-11 23:07:59 +0530

    Time: 5 ms (51.64%), Space: 23.9 MB (59.52%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        if(arr.empty()) return {};
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> res;
+        res.push_back(arr[0]);
+
+        for(int i=1;i<arr.size();++i){
+            if(arr[i][0] <= res.back()[1]){
+                res.back()[1] = max(res.back()[1], arr[i][1]);
+            }else{
+                res.push_back(arr[i]);
+            }
+        }
+        return res;
+    }
+};
